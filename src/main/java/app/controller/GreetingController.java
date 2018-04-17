064cb5e47567ab4ee7ed602891172bfb4b70ffbe
afsinka <afsinka@gmail.com>
2018-04-17 20:26:35 +0300

    add log dto

--- src/main/java/app/controller/GreetingController.java
+++ src/main/java/app/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package app.controller;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.logging.log4j.LogManager;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import app.model.Greeting;
+import app.model.LogDTO;
 
 @RestController
 public class GreetingController {
@@ -19,9 +24,22 @@ public class GreetingController {
 	private static final Logger logger = LogManager.getLogger(GreetingController.class);
 
 	@RequestMapping("/greeting")
+
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		logger.info("Hello log for: " + name);
+
+		LogDTO test = new LogDTO();
+		test.setKey(UUID.randomUUID().toString());
+		test.setValue(name);
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			logger.info(mapper.writeValueAsString(test));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
 	}
 
 }
